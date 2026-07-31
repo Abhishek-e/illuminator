4254c0edce16f3960933839f8b2fd2974a98dd5c
Abhishek Mondal <70214469+Abhishek-e@users.noreply.github.com>
2026-08-01 05:18:30 +0530

    Refactor vite.config.js to simplify configuration

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-export default defineConfig(({ command }) => ({
-  base: command === 'build' ? '/illuminator/' : '/',
+export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 5173,
-  },
-}))
+})
